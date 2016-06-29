3c1dfb4608ba20282c18b155d76aeaa956bec2b6
pnandedkar <pnandedkar@VSPL1115CPU10.co.valudconsulting.com>
2016-06-29 15:23:06 +0530

    String 5 assignment 28/06/2016

---
+++ String5_assignment/src/pkg/Application.java
@@ -0,0 +1,18 @@
+package pkg;
+
+public class Application 
+{
+	// 5. Write an application that reads several lines of text and prints a
+	// table indicating the number of occurrences of each different word in the
+	// text. The first version of your program should include the words in the
+	// table in the same order in which they appear in the text.
+	
+	public static void main(String[] args) 
+	{
+		Blogic bl = new Blogic();
+		bl.GetData();
+		bl.Print();
+
+	}
+
+}
